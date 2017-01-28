98f0504b91e7b7b91ed9bd8e602483f20e50c859
alu0100836059 <alu0100836059@ull.edu.es>
2017-01-28 14:58:49 +0000

    +intro.html

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
-app.get('/about', function(req, res){
-  res.sendfile(__dirname + '/public/about.html');
+app.get('/intro', function(req, res){
+  res.sendFile(__dirname + '/public/pages/intro.html');
 });
+
 
 // puerto
 var port = process.env.PORT || 8080;
