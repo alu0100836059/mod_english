eedf849588ed9ab249e82bb395bff24ad447b820
alu0100836059 <alu0100836059@ull.edu.es>
2017-01-28 05:18:32 +0000

    Linking css

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+'use strict'
+
 // importar
 var express = require('express');
 
 // instanciar
 var app = express();
 
+console.log("Principio");
+// app.set('public', __dirname + '/public');
+// app.set('view engine', 'ejs');
+
+
 // ruteo
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
+  console.log("/");
 });
 app.get('/about', function(req, res){
   res.sendfile(__dirname + '/public/about.html');
@@ -15,6 +23,8 @@ app.get('/about', function(req, res){
 // puerto
 var port = process.env.PORT || 8080;
 // escuchar
-app.listen(port);
+// app.listen(port);
+app.listen(8080);
 //
 // console.log("Servidor Express escuchando en modo %s", app.settings.env);
+module.exports = app
