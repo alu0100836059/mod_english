0d96d5a26a35d98ba8186257743c769137a5b60b
alu0100836059 <alu0100836059@ull.edu.es>
2017-01-28 15:48:25 +0000

    añadido login insertado

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 
-// ruteo
+//------------------------ RUTAS ------------------------------//
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
@@ -18,6 +18,10 @@ app.get('/intro', function(req, res){
   res.sendFile(__dirname + '/public/pages/intro.html');
 });
 
+app.get('/login', function(req, res){
+  res.sendFile(__dirname + '/public/pages/login.html')
+})
+//----------------- FINAL RUTAS ------------------------------//
 
 // puerto
 var port = process.env.PORT || 8080;
